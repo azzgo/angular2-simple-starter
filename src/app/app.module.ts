38a2f5059baaa1b1bd737c1873a3591b40cf7704
Yishan Wen <wittywys@gmail.com>
2017-06-21 08:00:22 +0800

    add assets of todoMVC here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 
+// import css
+
+import 'todomvc-common/base.css';
+import 'todomvc-app-css/index.css';
+
 @NgModule({
   imports: [BrowserModule, AppRoutingModule],
   exports: [],
